c2ac29d6d1762c58ad5ef33e9e44e81e11a9c408
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2023-11-13 15:16:08 +0100

    Added Orleans Host

---
+++ src/OrleansVsAkka.VisugXL/OrleansVsAkka.VisugXL.Orleans.Host/Program.cs
@@ -0,0 +1,10 @@
+using OrleansVsAkka.VisugXL.Orleans.Host;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();

213abb1e2fad3485823feffda38ead182bfed268
Hannes Lowette <lowette.hannes@gmail.com>
2023-11-30 14:55:42 +0100

    Last commit pre VISUG

--- src/OrleansVsAkka.VisugXL/OrleansVsAkka.VisugXL.Orleans.Host/Client.cs
+++ src/OrleansVsAkka.VisugXL/OrleansVsAkka.VisugXL.Orleans.Host/Client.cs
@@ -2,16 +2,12 @@
 
 public class Client
 {
-    private readonly IClusterClient _clusterClient;
-
-    public Client(IClusterClient clusterClient)
+    public Client()
     {
-        _clusterClient = clusterClient;
     }
 
     public async Task DoSomething()
     {
-        var grain = _clusterClient.GetGrain<IDeatchmatchGrain>(Guid.NewGuid());
-        var result = await grain.HandleCompleteRound(new CompleteRoundContent(1));
+
     }
 }
--- src/OrleansVsAkka.VisugXL/OrleansVsAkka.VisugXL.Orleans.Host/DeathmatchGrain.cs
+++ src/OrleansVsAkka.VisugXL/OrleansVsAkka.VisugXL.Orleans.Host/DeathmatchGrain.cs
@@ -1,34 +1,11 @@
 ﻿namespace OrleansVsAkka.VisugXL.Orleans.Host;
 
 
-[GenerateSerializer]
 public record CompleteRoundContent(int Number);
 
-[GenerateSerializer]
 public record RoundCompletedContent(int Number);
 
-
-
-
-public interface IDeatchmatchGrain : IGrainWithGuidKey
+public class DeatchmatchGrain
 {
-    Task<RoundCompletedContent> HandleCompleteRound(CompleteRoundContent content);
-}
 
-
-
-
-public class DeatchmatchGrain : Grain, IDeatchmatchGrain
-{
-    public async Task<RoundCompletedContent> HandleCompleteRound(CompleteRoundContent content)
-    {
-        // Write your code here...
-
-        var grain = GrainFactory.GetGrain<IAnotherGrain>(Guid.NewGuid());
-        await grain.Something();
-
-        await Task.Delay(Random.Shared.Next(1000));
-
-        return new RoundCompletedContent(content.Number);
-    }
 }

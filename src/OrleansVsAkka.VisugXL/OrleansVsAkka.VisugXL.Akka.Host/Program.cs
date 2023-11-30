a922bcdfc499b36609789f99c2f574004d693e54
Hannes Lowette <lowette.hannes@gmail.com>
2023-11-30 14:12:16 +0100

    Working Akka solution

--- src/OrleansVsAkka.VisugXL/OrleansVsAkka.VisugXL.Akka.Host/Program.cs
+++ src/OrleansVsAkka.VisugXL/OrleansVsAkka.VisugXL.Akka.Host/Program.cs
@@ -1,10 +1,1 @@
-﻿namespace OrleansVsAkka.VisugXL.Akka.Host
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
